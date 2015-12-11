4487579fbd31cac6a0344cb9a58ac6f1f5b102a7
naner121 <17090407852zzq@gmail.com>
2015-12-11 22:25:01 +0800

    Initial Commit

---
+++ iOS_SN_Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_SN_Animation
+//
+//  Created by kiwi on 15/12/11.
+//  Copyright © 2015年 kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_SN_Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_SN_Animation
+//
+//  Created by kiwi on 15/12/11.
+//  Copyright © 2015年 kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
